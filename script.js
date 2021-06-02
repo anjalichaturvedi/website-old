851b23bde1ce476b8d966388ac1eec0dc1afc7b8
Anjali Chaturvedi <anjaliichaturvedi@gmail.com>
2021-06-02 14:38:17 +0530

    added files

--- script.js
+++
@@ -1,9 +0,0 @@
-/* Adds scroll bar to show progress of page scroll. */
-window.onscroll = function() {handleOnScroll()};
-
-function handleOnScroll() {
-  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-  var scrolled = (winScroll / height) * 100;
-  document.getElementById("myBar").style.width = scrolled + "%";
-}
